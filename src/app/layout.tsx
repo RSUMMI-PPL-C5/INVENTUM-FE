bbd4ab5df525c4047c806528ac01e91222321de2
azmyar <azmy.arya.rizaldi@gmail.com>
2025-03-10 22:15:46 +0700

    chore: tiggering sonar scanning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "INVENTUM",
@@ -14,10 +15,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>
-                {children}
-                <Toaster position="top-center" richColors/>
-            </body>
+            <Suspense>
+                <body>
+                    {children}
+                    <Toaster position="top-center" richColors/>
+                </body>
+            </Suspense>
 		</html>
 	);
 }
