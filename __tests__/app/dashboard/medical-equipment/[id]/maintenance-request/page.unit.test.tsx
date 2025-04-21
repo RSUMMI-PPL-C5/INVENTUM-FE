0695f62af3af2ba45c6f60bf60e2d858f24e0f62
fikririsyad <fikri.risyad10@gmail.com>
2025-04-21 23:26:28 +0700

    [RED] Add tests for core page elements

--- __tests__/app/dashboard/medical-equipment/[id]/maintenance-request/page.unit.test.tsx
+++ __tests__/app/dashboard/medical-equipment/[id]/maintenance-request/page.unit.test.tsx
@@ -1,10 +1,20 @@
 import { render, screen } from '@testing-library/react';
-import MaintenanceRequestCreatePage from '@/app/dashboard/medical-equipment/[id]/maintenance-request/page';
+import MaintenanceRequestCreate from '@/modules/medical-equipment/request/maintenance/maintenance-request-create';
+
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+  useParams: jest.fn().mockReturnValue({ id: 'dummy-id' }),
+}));
 
 describe('MaintenanceRequestCreatePage', () => {
-  it('', () => {
-    render(<MaintenanceRequestCreatePage />);
-    const pageName = screen.getByText('MaintenanceRequestCreate');
-    expect(pageName).toBeInTheDocument();
+  it('renders the page correctly', () => {
+    render(<MaintenanceRequestCreate />);
+    
+    expect(screen.getByText('Kembali')).toBeInTheDocument();
+    expect(screen.getByText('Minta Maintenance')).toBeInTheDocument();
+    expect(screen.getByLabelText(/Alat/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Catatan/i)).toBeInTheDocument();
+    expect(screen.getByText('Batalkan')).toBeInTheDocument();
+    expect(screen.getByText('Simpan')).toBeInTheDocument();
   });
 });
