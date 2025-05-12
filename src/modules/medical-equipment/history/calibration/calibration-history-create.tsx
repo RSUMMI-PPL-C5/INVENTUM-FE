4108397ac1fd47882d9ada5764a444abc66cfdd1
fauzanjaisyu <fauzanjaisyu@gmail.com>
2025-05-12 22:55:16 +0700

    [REFACTOR] Implent new dropdown options for FE

--- src/modules/medical-equipment/history/calibration/calibration-history-create.tsx
+++ src/modules/medical-equipment/history/calibration/calibration-history-create.tsx
@@ -40,6 +40,8 @@ const resultOptions = [
   { id: "Success", name: "Berhasil" },
   { id: "Partial", name: "Sebagian" },
   { id: "Failed", name: "Gagal" },
+  { id: "Success with Issues", name: "Berhasil dengan Catatan" },
+  { id: "Failed with Issues", name: "Gagal dengan Catatan" },
 ]
 
 export default function CalibrationHistoryCreate() {
