cb2661d3c09dbf993f16f771ae6ff8720c650918
azmyar <azmy.arya.rizaldi@gmail.com>
2025-05-22 06:02:07 +0700

    chore: lint

--- src/modules/spare-part/spare-part-details.tsx
+++ src/modules/spare-part/spare-part-details.tsx
@@ -25,17 +25,6 @@ import {
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Badge } from "@/components/ui/badge";
-import {
-	AlertDialog,
-	AlertDialogAction,
-	AlertDialogCancel,
-	AlertDialogContent,
-	AlertDialogDescription,
-	AlertDialogFooter,
-	AlertDialogHeader,
-	AlertDialogTitle,
-	AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 import DeleteDialog from "@/components/general/delete-dialog";
 
 type Sparepart = {
